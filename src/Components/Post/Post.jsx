bb2537ce62376de82bf0dee2de24249082ac8397
mildk123 <mildk123@gmail.com>
2018-10-03 18:25:30 +0500

    Update Post.JSX (Added likes)

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-// import FbImageLibrary from 'react-fb-image-grid';
-import FbImageLibrary from '../Images'
+import FbImageLibrary from 'react-fb-image-grid';
+import FacebookEmoji from 'react-facebook-emoji';
+import Glyphicon from 'react-bootstrap';
 import '../../CSS/Post.css'
 
 
@@ -10,6 +11,9 @@ const images = [
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2QpuZBjR8wPwKUleiQteTozL1Kl95_HzSx2STyYSLKNFpiVli',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp5KErePBeTqsPjQJ-yrTlm7UdYGaMMjgQ6qgYVCJmP0wE6zJg',
     'http://www.voyagecharters.com/media//magpleasure/imagegallery/file_name_file/0/2/021a845ff10925a89c802cdaca844de8.jpg',
+    'http://www.voyagecharters.com/media//magpleasure/imagegallery/file_name_file/0/2/021a845ff10925a89c802cdaca844de8.jpg',
+    // 'http://www.voyagecharters.com/media//magpleasure/imagegallery/file_name_file/0/2/021a845ff10925a89c802cdaca844de8.jpg',
+
 ]
 
 class Post extends Component {
@@ -38,33 +42,36 @@ class Post extends Component {
                     {/* MID SECTION OF THE POST */}
                     {/* ----------------------------------------------------- */}
                     <div id='images-mid'>
-                        <FbImageLibrary images={images} countFrom={5} width={100} />
+                        <FbImageLibrary images={images} countFrom={5} width={100} hideOverlay={true} />
                     </div>
 
 
                     {/* BOTTOM SECTION OF THE POST */}
                     {/* ----------------------------------------------------- */}
-                    <div>
+                    <div id='interact-bottom'>
                         <div id="bottom-top-row">
                             <div id="reactions">
-
+                            <FacebookEmoji type="like" size="xxs"/>
+                            <FacebookEmoji type="love" size="xxs"/>
+                            <FacebookEmoji type="wow" size="xxs"/>
                             </div>
                             <div id="likes-list">
-
+                                <p>Apple, toffer, popye and 132 others likes this</p>
                             </div>
                             <div id="comments-list">
-
+                                <p>123 comments</p>
                             </div>
                         </div>
                         <div id="bottom-bottom-row">
                             <div id="like-btn">
-
+                            <Glyphicon glyph="star" />
+                                <input type='button' value="Like" />
                             </div>
                             <div id="comment-btn">
-
+                                <input type='button' value="Comment" />
                             </div>
                             <div id="share-btn">
-
+                                <input type='button' value="Share" />
                             </div>
 
                         </div>
