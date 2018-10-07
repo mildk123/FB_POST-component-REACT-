119d54165020d86a135d7272e9bb29c0b6bcd541
mildk123 <mildk123@gmail.com>
2018-10-07 08:31:29 +0500

    only REACTIONS LEFT

--- src/App.js
+++ src/App.js
@@ -5,25 +5,56 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            createdBy: `Somebody's Name`,
+            createdBy: `Milad Khan`,
             avatar: `https://www.clevelanddentalhc.com/wp-content/uploads/2018/03/sample-avatar.jpg`,
-            description: `I'm not feeling good today!`,
+            description: `Who's Up?`,
             images: [
                 'https://s3.amazonaws.com/secretsaucefiles/photos/images/000/107/867/large/Samples.jpeg?1485369806',
                 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9p3NDZJgywrwhR1dqSBRKshGf2P9cTDI4yM2JIAQ3NLNQeYNy',
                 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2QpuZBjR8wPwKUleiQteTozL1Kl95_HzSx2STyYSLKNFpiVli',
                 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp5KErePBeTqsPjQJ-yrTlm7UdYGaMMjgQ6qgYVCJmP0wE6zJg',
                 'http://www.voyagecharters.com/media//magpleasure/imagegallery/file_name_file/0/2/021a845ff10925a89c802cdaca844de8.jpg',
-                'http://www.voyagecharters.com/media//magpleasure/imagegallery/file_name_file/0/2/021a845ff10925a89c802cdaca844de8.jpg',],
+                'http://www.voyagechartnpm ers.com/media//magpleasure/imagegallery/file_name_file/0/2/021a845ff10925a89c802cdaca844de8.jpg',],
             createdAt: Date.now(),
-            likes: [`Milad Khan`, 'Faizan Ahmed', 'Kashif Suleiman'],
-        };
+            likes: 
+            [
+            "Ahmed",
+            "Ali",
+            "Abbas",
+            "Hasan",
+            "Hussain",
+            "Alam",
+            "Umer",
+            "Saif"
+        ],
+            likedByMe: false    
+    };
+
+        this.likeFunction = this.likeFunction.bind(this);
     }
+
+    likeFunction(){
+        let likesArray = this.state.likes;
+        let includesResp = likesArray.includes('You');
+        if (!includesResp){
+            likesArray.unshift('You')
+        }else{
+            likesArray.shift()
+        }
+
+        this.setState({
+            likes : likesArray,
+            likedByMe : !this.state.likedByMe
+        })
+
+    }
+
+
 
     render() {
         return (
             <div>
-                <Post state={this.state} />
+                <Post likeFunction={this.likeFunction} state={this.state} />
             </div>
         );
     }
--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import FbImageLibrary from 'react-fb-image-grid';
+import FacebookEmoji from 'react-facebook-emoji';
+import { Glyphicon } from 'react-bootstrap';
 import swal from 'sweetalert';
+import '../../CSS/Post.css'
 
 
 
@@ -10,15 +14,86 @@ class Post extends Component {
     }
 
 
-CommingSoon(){
-    swal("Function Coming Soon!", "Please Be Patient!", "warning");
-    
-}
-    
+    CommingSoon() {
+        swal("Function Coming Soon!", "Please Be Patient!", "warning");
+
+    }
+
 
     render() {
-        console.log(this.props.state)
-        const { createdBy, avatar, description, images, likes } = this.props.state;
+        const { createdBy, avatar, description, images, likes, likedByMe } = this.props.state;
+        const { likeFunction } = this.props;
+
         return (
             <div>
-               
+                <div id="post">
+                    {/* TOP SECTION OF THE POST */}
+                    {/* ----------------------------------------------------- */}
+                    <div id="post-header">
+                        <div id="top-top-row">
+                            <div id="dp-row">
+                                <img src={avatar} alt="profile-pic" />
+                            </div>
+                            <div id="name-row">
+                                <p>{createdBy}</p>
+                            </div>
+
+                            <div id="desc-row">
+                                <p>{description}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* MID SECTION OF THE POST */}
+                    {/* ----------------------------------------------------- */}
+                    <div id='images-mid'>
+                        <FbImageLibrary width={100} images={images} hideOverlay={true} />
+                    </div>
+                    {/* BOTTOM SECTION OF THE POST */}
+                    {/* ----------------------------------------------------- */}
+                    <div id='interact-bottom'>
+                        <div id="bottom-top-row">
+                            <div id="reactions">
+                                <FacebookEmoji type="like" size="xxs" />
+                                <FacebookEmoji type="love" size="xxs" />
+                                <FacebookEmoji type="wow" size="xxs" />
+                            </div>
+                            <div id="likes-list">
+                                {likes.length > 4 ?`${likes[0]},${likes[1]} and ${likes.length - 2} others liked this.`:
+                                    likes.map(item => {
+                                            return <span key={item}>{item},</span>
+                                        })
+                                }
+                            </div>
+                            <div id="comments-list">
+                                <p onClick={this.CommingSoon}>123 comments</p>
+                            </div>
+                        </div>
+                        <div id="bottom-bottom-row">
+                            <div id="like-btn">
+                                {!likedByMe && <Glyphicon glyph="glyphicon glyphicon-thumbs-up" style={{ margin: 6 }} />}
+                                {!likedByMe && <input type='button' onClick={likeFunction} value="Like" />}
+                                {likedByMe && <Glyphicon glyph="glyphicon glyphicon-thumbs-up" style={{ margin: 6, color: 'blue' }} />}
+                                {likedByMe && <input type='button' onClick={likeFunction} value="Like" style={{ color: 'blue' }} />}
+                            </div>
+                            <div id="comment-btn">
+                                <Glyphicon glyph="glyphicon glyphicon-comment" style={{ margin: 6 }} />
+                                <input type='button' onClick={this.CommingSoon} value="Comment" />
+                            </div>
+                            <div id="share-btn">
+                                <Glyphicon glyph="glyphicon glyphicon-share" style={{ margin: 6 }} />
+                                <input type='button' onClick={this.CommingSoon} value="Share" />
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Post;
+
+
